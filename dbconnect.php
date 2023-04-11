1ce19ec45f490f58921589a1fe2401ab6bdf2593
Hipol1to <hipolitoprz2001@gmail.com>
2023-04-10 20:36:33 -0400

    final commit

--- dbconnect.php
+++ dbconnect.php
@@ -1,8 +1,8 @@
 <?php 
 define('DB_HOST', 'localhost'); 
-define('DB_NAME', 'bookstore'); 
-define('DB_USER','root'); 
-define('DB_PASSWORD',''); 
+define('DB_NAME', 'id18282910_bookstore'); 
+define('DB_USER','id18282910_webb'); 
+define('DB_PASSWORD',']G|v>g~IC_4bEk08'); 
 
 $con=mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysql_error()); 
 $db=mysqli_select_db($con,DB_NAME) or die("Failed to connect to MySQL: " . mysql_error()); 
